4c8d8317a8c39823b6083d713f77830377383472
Chris <chrisherb.sites@outlook.com>
2023-11-10 08:59:10 +0100

    add custom colors to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      "trnr-primary": "#87DEAA",
+      "trnr-secondary": "#F55A50",
+      "trnr-background": "black",
+    },
     extend: {},
   },
   plugins: [],
