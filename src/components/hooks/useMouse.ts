9215eb90928e9971049d3c2f3e161db118b5c21f
Chris <chrisherb.sites@outlook.com>
2023-11-16 11:44:25 +0100

    optimizing effects

--- src/components/hooks/useMouse.ts
+++ src/components/hooks/useMouse.ts
@@ -1,19 +1,24 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 export const useMouse = (onMouseUp?: () => void) => {
   const [posX, setPosX] = useState(0);
   const [posY, setPosY] = useState(0);
 
-  const handleMouseMove = (ev: MouseEvent) => {
-    setPosX(ev.clientX);
-    setPosY(ev.clientY);
-  };
+  const handleMouseMove = useCallback(
+    (ev: MouseEvent) => {
+      setPosX(ev.clientX);
+      setPosY(ev.clientY);
+    },
+    [setPosX, setPosY]
+  );
 
   useEffect(() => {
+    console.log("register mousemove");
     window.addEventListener("mousemove", handleMouseMove);
-  }, []);
+  }, [handleMouseMove]);
 
   useEffect(() => {
+    console.log("register mouseup");
     if (onMouseUp) window.addEventListener("mouseup", onMouseUp);
   }, [onMouseUp]);
 
