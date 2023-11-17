21a3001d72191c3c45046ca358fb8d09286349ac
Chris <chrisherb.sites@outlook.com>
2023-11-17 11:13:47 +0100

    use library mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    lib: {
+      entry: resolve(__dirname, "components/index.ts"),
+      name: "TrnrUI",
+      fileName: "trnr-ui",
+    },
+    rollupOptions: {
+      external: ["react"],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          react: "React",
+        },
+      },
+    },
+  },
+});
