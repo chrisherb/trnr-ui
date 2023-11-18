d936592f40136dee948c425e5c4bf61b59bdaaf7
Chris <chrisherb.sites@outlook.com>
2023-11-18 11:22:09 +0100

    cleanup

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,6 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     colors: {
-      // "primary": "#87DEAA",
-      // "secondary": "#F55A50",
-      // "background": "black",
       primary: "var(--color-primary)",
       secondary: "var(--color-secondary)",
       background: "var(--color-background)",
