7f8507f6edcb542c63e443e772a6425856b5ddb3
Chris <chrisherb.sites@outlook.com>
2023-11-18 11:15:06 +0100

    global exports

---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+import Dial from "./controls/Dial";
+import Slider from "./controls/Slider";
+import Grid from "./layout/Grid";
+import GridCell from "./layout/GridCell";
+import Trnr from "./layout/Trnr";
+
+export { Trnr, Grid, GridCell, Dial, Slider };
