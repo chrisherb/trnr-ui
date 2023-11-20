a5ed8f3a47f7a6334ab55b6053145085214d1477
Chris <chrisherb.sites@outlook.com>
2023-11-20 14:25:22 +0100

    export button

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,8 @@
+import Button from "./controls/Button";
 import Dial from "./controls/Dial";
 import Slider from "./controls/Slider";
 import Grid from "./layout/Grid";
 import GridCell from "./layout/GridCell";
 import Trnr from "./layout/Trnr";
 
-export { Trnr, Grid, GridCell, Dial, Slider };
+export { Trnr, Grid, GridCell, Dial, Slider, Button };
