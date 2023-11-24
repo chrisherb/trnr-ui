5d686ce54da4d06f2a3ead278fdeadd6e105b6e1
Chris <chrisherb.sites@outlook.com>
2023-11-24 10:09:08 +0100

    styled dropdown with headlessui

--- src/components/controls/Dropdown.tsx
+++ src/components/controls/Dropdown.tsx
@@ -1,5 +1,7 @@
-import Frame from "./Frame";
+import { Listbox } from "@headlessui/react";
 import Stack from "./Stack";
+import { useState } from "react";
+import Frame from "./Frame";
 
 interface DropdownProps {
   label?: string;
@@ -7,15 +9,30 @@ interface DropdownProps {
 }
 
 const Dropdown = ({ label, options }: DropdownProps) => {
+  const [selectedOption, setSelectedOption] = useState(options[0]);
+
   return (
     <Stack header={<span>{label}</span>}>
-      <Frame>
-        <select className="w-full h-full bg-background uppercase p-1">
-          {options.map((option, index) => (
-            <option key={index}>{option}</option>
-          ))}
-        </select>
-      </Frame>
+      <div className="relative w-full h-full">
+        <Listbox value={selectedOption} onChange={setSelectedOption}>
+          <Frame>
+            <Listbox.Button className="rounded-sm uppercase w-full h-full hover:bg-secondary hover:text-background">
+              {selectedOption}
+            </Listbox.Button>
+          </Frame>
+          <Listbox.Options className="border-global mt-2 border-primary bg-background rounded-md absolute w-full h-full p-global uppercase flex flex-col justify-center">
+            {options.map((option, index) => (
+              <Listbox.Option
+                key={index}
+                value={option}
+                className="flex-grow hover:bg-secondary hover:text-background flex content-center"
+              >
+                {option}
+              </Listbox.Option>
+            ))}
+          </Listbox.Options>
+        </Listbox>
+      </div>
     </Stack>
   );
 };
