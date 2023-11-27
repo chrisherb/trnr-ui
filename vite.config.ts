4952d3b8633140ea6a45cb3f26f8f384092a9cba
Chris <chrisherb.sites@outlook.com>
2023-11-27 11:26:35 +0100

    explicitly emit an index.html file

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,29 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
+import * as fs from "fs";
+import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/trnr-ui",
-  plugins: [react()],
+  plugins: [
+    // explicitly emit an index.html file for demo purposes
+    {
+      name: "emit-index",
+      generateBundle() {
+        this.emitFile({
+          type: "asset",
+          fileName: "index.html",
+          source: fs.readFileSync(
+            path.resolve(__dirname, "index.html"),
+            "utf-8"
+          ),
+        });
+      },
+    },
+    react(),
+  ],
   build: {
     lib: {
       entry: resolve(__dirname, "src/components/index.ts"),
