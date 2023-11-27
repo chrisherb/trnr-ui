3ef9977d41fa3d55dc95a6911167220e53441cf0
Chris <chrisherb.sites@outlook.com>
2023-11-27 13:04:30 +0100

    separate vite config for building docs

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,8 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 import { resolve } from "path";
-import * as fs from "fs";
-import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/trnr-ui",
-  plugins: [
-    // explicitly emit an index.html file for demo purposes
-    {
-      name: "emit-index",
-      generateBundle() {
-        this.emitFile({
-          type: "asset",
-          fileName: "index.html",
-          source: fs.readFileSync(
-            path.resolve(__dirname, "index.html"),
-            "utf-8"
-          ),
-        });
-      },
-    },
-    react(),
-  ],
   build: {
     lib: {
       entry: resolve(__dirname, "src/components/index.ts"),
@@ -31,6 +10,7 @@ export default defineConfig({
       fileName: "trnr-ui",
     },
     rollupOptions: {
+      input: { main: resolve(__dirname, "index.html") },
       external: ["react"],
       output: {
         // Provide global variables to use in the UMD build
