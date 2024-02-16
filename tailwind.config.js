6c7c32dbf0408202128d247cd3bfbd9f2a921eb3
Chris <chris@ternar.tech>
2024-02-16 11:10:39 +0100

    initial daisy ui implementation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,46 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    colors: {
-      primary: "var(--trnr-color-primary)",
-      secondary: "var(--trnr-color-secondary)",
-      background: "var(--trnr-color-background)",
-      transparent: "rgba(0, 0, 0, 0)",
-    },
-    strokeWidth: { global: "var(--trnr-thickness)" },
-    fontFamily: {
-      sans: ["Jura"],
-    },
-    borderWidth: {
-      1: "var(--trnr-thickness)",
-    },
-    borderColor: {
-      primary: "var(--trnr-color-primary)",
-      secondary: "var(--trnr-color-secondary)",
-    },
-    borderRadius: {
-      1: "var(--trnr-roundness)",
-    },
-    ringOffsetWidth: {
-      1: "var(--trnr-thickness)",
-      4: "calc(var(--trnr-thickness) * 4)",
-    },
-    ringOffsetColor: {
-      primary: "var(--trnr-color-primary)",
-      secondary: "var(--trnr-color-secondary)",
-      background: "var(--trnr-color-background)",
-    },
-    extend: {
-      padding: {
-        roundness: "var(--trnr-roundness)",
-      },
-      height: {
-        9: "calc(var(--trnr-thickness) * 9)",
-      },
-      dropShadow: {
-        glow: "35px 35px 35px rgba(--trnr-color-primary)",
-      },
-    },
-  },
+  plugins: [require("daisyui")],
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,8 @@
 import { defineConfig } from "vite";
-import { resolve } from "path";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    lib: {
-      entry: resolve(__dirname, "src/components/index.ts"),
-      name: "TrnrUI",
-      fileName: "trnr-ui",
-    },
-    rollupOptions: {
-      input: { main: resolve(__dirname, "index.html") },
-      external: ["react"],
-      output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
-        globals: {
-          react: "React",
-        },
-      },
-    },
-  },
+  base: "/trnr-ui/",
+  plugins: [react()],
 });
