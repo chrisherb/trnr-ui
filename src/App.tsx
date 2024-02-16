7dd7e2f68795a9b0688d3b9bb96cac9e1dc1a2c0
Chris <chris@ternar.tech>
2024-02-16 12:57:13 +0100

    controls + details list

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import { useState } from "react";
+
 function App() {
   return (
     <div className="h-screen flex">
       <div className="w-96 border-r border-neutral">
+        <Controls items={["Item 1", "Item 2", "Item 3", "Item 4"]} />
+        <Details />
         <Navbar />
-        <div className="divider"></div>
       </div>
       <div className="flex-auto"></div>
     </div>
@@ -17,6 +20,28 @@ function Navbar() {
       <button className="btn btn-neutral">Save</button>
       <button className="btn btn-neutral">Export</button>
     </div>);
+}
+
+function Controls(props: { items: string[] }) {
+  const [active, setActive] = useState("");
+  return (
+    <ul className="menu">
+      <li className="menu-title">Controls</li>
+      {props.items.map(item =>
+        <li>
+          <a className={item === active ? "active" : ""} onClick={() => setActive(item)}>{item}</a>
+        </li>)}
+    </ul>
+  )
+}
+
+function Details() {
+  return (
+    <div role="tablist" className="tabs tabs-bordered">
+      <a role="tab" className="tab">Common</a>
+      <a role="tab" className="tab tab-active">Stuff</a>
+    </div>
+  );
 }
 
 export default App;
