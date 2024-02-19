8a4367e336360c97b4fdf79bd3c7614d540f94ab
Chris <chris@ternar.tech>
2024-02-19 13:14:31 +0100

    svg viewer renders panel

--- src/components/SvgViewer.tsx
+++ src/components/SvgViewer.tsx
@@ -5,6 +5,22 @@ export function SvgViewer(props: { conf: UIConfig }) {
     <div className="w-full h-full flex justify-center items-center">
       <svg width={props.conf.width} height={props.conf.height}>
         <rect width="100%" height="100%" fill="black" />
+        {props.conf.controls.map((control) => {
+          if (control.name === "Panel") {
+            return (
+              <rect
+                x={control.x}
+                y={control.y}
+                width={control.width}
+                height={control.height}
+                fill="none"
+                stroke="red"
+                rx={5}
+                ry={5}
+              />
+            );
+          }
+        })}
       </svg>
     </div>
   );
