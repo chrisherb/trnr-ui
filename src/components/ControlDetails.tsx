b8bcc54987b95eb0e25abf5a81eaeee16c6934d3
Chris <chris@ternar.tech>
2024-02-19 09:42:25 +0100

    clickable tabs

--- src/components/ControlDetails.tsx
+++ src/components/ControlDetails.tsx
@@ -1,10 +1,22 @@
+import { useState } from "react";
+
 export function ControlDetails() {
+  const [activeTab, setActiveTab] = useState(0);
+
   return (
     <div role="tablist" className="tabs tabs-bordered">
-      <a role="tab" className="tab">
+      <a
+        role="tab"
+        className={`tab ${activeTab === 0 && "tab-active"}`}
+        onClick={() => setActiveTab(0)}
+      >
         Common
       </a>
-      <a role="tab" className="tab tab-active">
+      <a
+        role="tab"
+        className={`tab ${activeTab === 1 && "tab-active"}`}
+        onClick={() => setActiveTab(1)}
+      >
         Stuff
       </a>
     </div>
